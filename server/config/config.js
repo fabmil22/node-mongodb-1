bdd3499557c061799a28f47396088d3c26843c3e
unknown <fabmil22@gmail.comm>
2019-08-17 21:15:09 +0200

    fixed

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ process.env.PORT =   process.env.PORT || 3000;
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
-conection = ( process.env.NODE_ENV  === 'dev') ? 'mongodb://127.0.0.1:27017/cafe' :'mongodb+srv://fab:c4r4c4s@cluster0-hspno.gcp.mongodb.net/test?retryWrites=true&w=majority'
+conection = ( process.env.NODE_ENV  === 'dev') ? 'mongodb://127.0.0.1:27017/cafe' :'mongodb+srv://fab:c4r4c4s@cluster0-hspno.gcp.mongodb.net/cafe?retryWrites=true&w=majority'
 
 
 
